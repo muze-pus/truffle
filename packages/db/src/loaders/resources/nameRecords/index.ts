lapolinar0519/lapolinar-truffle

Move contract namekeeping to new loaders system
Define and use new loaders:
  - generateNameRecordsLoad
  - generateProjectNameResolve
  - generateProjectNamesAssign

packages/db/src/loaders/resources/nameRecords/index.ts
@@ -1,2 +1,62 @@
+import {
+  WorkspaceRequest,
+  IdObject,
+  toIdObject,
+  WorkspaceResponse
+} from "@truffle/db/loaders/types";
+
 import { AddNameRecords } from "./add.graphql";
 export { AddNameRecords };
+
+interface Resource {
+  id: string;
+  name: string;
+}
+
+type ResolveFunc = (
+  name: string,
+  type: string
+) => Generator<
+  WorkspaceRequest,
+  DataModel.INameRecord | null,
+  WorkspaceResponse
+>;
+
+export function* generateNameRecordsLoad(
+  resources: Resource[],
+  type: string,
+  getCurrent: ResolveFunc
+): Generator<
+  WorkspaceRequest,
+  DataModel.INameRecord[],
+  WorkspaceResponse<"nameRecordsAdd", DataModel.INameRecordsAddPayload>
+> {
+  const nameRecords = [];
+  for (const resource of resources) {
+    const { name } = resource;
+
+    const current: DataModel.INameRecord = yield* getCurrent(name, type);
+
+    if (current) {
+      nameRecords.push({
+        name,
+        type,
+        resource: toIdObject(resource),
+        previous: toIdObject(current)
+      });
+    } else {
+      nameRecords.push({
+        name,
+        type,
+        resource: toIdObject(resource)
+      });
+    }
+  }
+
+  const result = yield {
+    mutation: AddNameRecords,
+    variables: { nameRecords }
+  };
+
+  return result.data.workspace.nameRecordsAdd.nameRecords;
+}

packages/db/src/loaders/resources/projects/index.ts
@@ -25,3 +25,45 @@ export function* generateProjectLoad(
 
   return result.data.workspace.projectsAdd.projects[0];
 }
+
+export function* generateProjectNameResolve(
+  project: IdObject<DataModel.IProject>,
+  name: string,
+  type: string
+): Generator<
+  WorkspaceRequest,
+  DataModel.INameRecord,
+  WorkspaceResponse<"project", { resolve: DataModel.IProject["resolve"] }>
+> {
+  const result = yield {
+    mutation: ResolveProjectName,
+    variables: {
+      projectId: project.id,
+      name,
+      type
+    }
+  };
+
+  return result.data.workspace.project.resolve[0];
+}
+
+export function* generateProjectNamesAssign(
+  project: IdObject<DataModel.IProject>,
+  nameRecords: DataModel.INameRecord[]
+): Generator<
+  WorkspaceRequest,
+  void,
+  WorkspaceResponse<"projectNamesAssign", DataModel.IProjectNamesAssignPayload>
+> {
+  const projectNames = nameRecords.map(({ id, name, type }) => ({
+    project,
+    nameRecord: { id },
+    name,
+    type
+  }));
+
+  yield {
+    mutation: AssignProjectNames,
+    variables: { projectNames }
+  };
+}

muze-pus/truffle

packages/db/src/loaders/resources/nameRecords/index.ts
@@ -1,2 +1,62 @@
+import {
+  WorkspaceRequest,
+  IdObject,
+  toIdObject,
+  WorkspaceResponse
+} from "@truffle/db/loaders/types";
+
 import { AddNameRecords } from "./add.graphql";
 export { AddNameRecords };
+
+interface Resource {
+  id: string;
+  name: string;
+}
+
+type ResolveFunc = (
+  name: string,
+  type: string
+) => Generator<
+  WorkspaceRequest,
+  DataModel.INameRecord | null,
+  WorkspaceResponse
+>;
+
+export function* generateNameRecordsLoad(
+  resources: Resource[],
+  type: string,
+  getCurrent: ResolveFunc
+): Generator<
+  WorkspaceRequest,
+  DataModel.INameRecord[],
+  WorkspaceResponse<"nameRecordsAdd", DataModel.INameRecordsAddPayload>
+> {
+  const nameRecords = [];
+  for (const resource of resources) {
+    const { name } = resource;
+
+    const current: DataModel.INameRecord = yield* getCurrent(name, type);
+
+    if (current) {
+      nameRecords.push({
+        name,
+        type,
+        resource: toIdObject(resource),
+        previous: toIdObject(current)
+      });
+    } else {
+      nameRecords.push({
+        name,
+        type,
+        resource: toIdObject(resource)
+      });
+    }
+  }
+
+  const result = yield {
+    request: AddNameRecords,
+    variables: { nameRecords }
+  };
+
+  return result.data.workspace.nameRecordsAdd.nameRecords;
+}

packages/db/src/loaders/resources/projects/index.ts
@@ -25,3 +25,45 @@ export function* generateProjectLoad(
 
   return result.data.workspace.projectsAdd.projects[0];
 }
+
+export function* generateProjectNameResolve(
+  project: IdObject<DataModel.IProject>,
+  name: string,
+  type: string
+): Generator<
+  WorkspaceRequest,
+  DataModel.INameRecord,
+  WorkspaceResponse<"project", { resolve: DataModel.IProject["resolve"] }>
+> {
+  const result = yield {
+    request: ResolveProjectName,
+    variables: {
+      projectId: project.id,
+      name,
+      type
+    }
+  };
+
+  return result.data.workspace.project.resolve[0];
+}
+
+export function* generateProjectNamesAssign(
+  project: IdObject<DataModel.IProject>,
+  nameRecords: DataModel.INameRecord[]
+): Generator<
+  WorkspaceRequest,
+  void,
+  WorkspaceResponse<"projectNamesAssign", DataModel.IProjectNamesAssignPayload>
+> {
+  const projectNames = nameRecords.map(({ id, name, type }) => ({
+    project,
+    nameRecord: { id },
+    name,
+    type
+  }));
+
+  yield {
+    request: AssignProjectNames,
+    variables: { projectNames }
+  };
+}